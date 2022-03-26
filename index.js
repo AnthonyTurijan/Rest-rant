27e49232fd50b09718849dff08632a4969ef71d6
AnthonyTurijan <anthonyturijan@gmail.com>
2022-03-26 12:24:13 -0700

    Added delete and edit buttons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 //requiring express
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 //Express Settings
@@ -11,6 +12,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 
 //Importing Routers
 app.use('/places',require('./controllers/places'))
