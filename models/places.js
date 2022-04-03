89307768a3d893cfda1b2d3d8890a86a13b6e141
AnthonyTurijan <anthonyturijan@gmail.com>
2022-04-03 13:54:22 -0700

    Added Instance Helper

--- models/places.js
+++ models/places.js
@@ -12,21 +12,10 @@ const placeSchema = new mongoose.Schema({
   founded: Number
 })
 
+//creating helpers
+placeSchema.methods.showEstablished = function(){
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
 //exporting places.js
 module.exports = mongoose.model('Place', placeSchema)
-
-
-
-// module.exports = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: '/images/cake.jpg'
-//   }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: '/images/fruits2.jpg'
-//   }]
