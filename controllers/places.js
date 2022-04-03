6377274ece5d0b20e51fedfedf98bf939046d27d
AnthonyTurijan <anthonyturijan@gmail.com>
2022-04-03 14:40:52 -0700

    Added validation error message

--- controllers/places.js
+++ controllers/places.js
@@ -9,8 +9,9 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
   })
   .catch(err =>{
-    console.log(err)
+    console.log('err', err)
     res.render('error404')
+    
   })
 })
 
@@ -21,8 +22,18 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   .catch(err=>{
-    console.log('err', err)
-    res.render('error404')
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+  }
+  else {
+      res.render('error404')
+  }
   })
 })
 
