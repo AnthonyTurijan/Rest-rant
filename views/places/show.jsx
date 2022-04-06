2a21f5091b8aaee1440141d3e99ac190474486f6
AnthonyTurijan <anthonyturijan@gmail.com>
2022-04-06 16:56:41 -0700

    Implemented First Comment

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,6 +3,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data){
+    let comments =(
+        <h3 className="inactive" >
+            No comments yet!
+        </h3>
+    )
+    if(data.place.comments.length){
+        comments = data.place.comments.map(c=>{
+            return(
+                <div className='border' >
+                    <h2 className="rant" >{c.rant ? 'Rant! 😡' : 'Rave! 😻'} </h2>
+                    <h4>{c.content} </h4>
+                    <h3>
+                        <strong>- {c.author} </strong>
+                    </h3>
+                    <h4>Rating: {c.stars}  </h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -26,7 +45,7 @@ function show (data){
                         <br/>
                         <hr/>
                         <h2>Comments</h2>
-                        <p> No comments yet!</p>
+                        {comments}
                     
                         <br/>
                         <hr/>
@@ -46,3 +65,4 @@ function show (data){
 }
 
 module.exports = show
+
