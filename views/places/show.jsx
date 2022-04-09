075df8d3e2aa869e59a1678e9690eabd98723983
AnthonyTurijan <anthonyturijan@gmail.com>
2022-04-09 09:53:00 -0700

    Implemented edit and delete function

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,6 +8,25 @@ function show (data){
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
+      )
+      if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+          stars += '⭐️'
+        }
+        rating = (
+          <h3>
+            {stars} stars
+          </h3>
+        )}
     if(data.place.comments.length){
         comments = data.place.comments.map(c=>{
             return(
@@ -79,7 +98,7 @@ function show (data){
                     
                         <br/>
                         <hr/>
-                        <a href={`/places/${data.id}/edit`} className='btn btn-warning' >
+                        <a href={`/places/${data.place.id}/edit`} className='btn btn-warning' >
                             Edit
                         </a>
                         <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
